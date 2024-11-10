e8b0a33f75a314adb3aabd183fa37375316da8ed
nurul wahidah <nurulwahidah430@gmail.com>
2024-11-10 20:13:34 +0700

    add commit

---
+++ app/src/main/java/nurul/polbeng/ac/id/viewdata/DetailBuahActivity.kt
@@ -0,0 +1,20 @@
+package nurul.polbeng.ac.id.viewdata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import nurul.polbeng.ac.id.viewdata.databinding.ActivityDetailBuahBinding
+
+class DetailBuahActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBuahBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailBuahBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //membuat variabel dan di tangkap data dri intent
+        var namaBuah = intent.getStringExtra("nama")
+        var image = intent.getIntExtra("gambar",0)
+        //menangkap data intent dan di set ke layout
+        binding.tvNamaBuah.text = namaBuah
+        binding.imgBuah.setImageResource(image)
+    }
+}
